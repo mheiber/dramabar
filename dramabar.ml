7e3e645c153617c72957abab532d7f7e098b745c
Max Heiber <max.heiber@gmail.com>
2017-05-07 18:09:00 -0400

    make the bar nicer

--- dramabar.ml
+++ dramabar.ml
@@ -1,46 +1,44 @@
 let default_columns = 40
 
-let with_process_in cmd args f =
-    let path = ["/bin";"/usr/bin"] in
-    let cmd =
-        List.find Sys.file_exists (List.map (fun d -> Filename.concat d cmd) path)
-    in
-    let ic = Unix.open_process_in (cmd^" "^args) in
-    try
-        let r = f ic in
-        ignore (Unix.close_process_in ic) ; r
-    with exn ->
-        ignore (Unix.close_process_in ic) ; raise exn
+(* terminal stuff *)
 
 let get_terminal_columns () = let width, height = ANSITerminal.size() in width
-
-
-let rec str_n_times str n =
-    if n < 2 then str else str ^ (str_n_times str (n - 1));;
 
 let clear_line () =
     ANSITerminal.(
         set_cursor 1 (* row *) (-1) (* no change to col *);
         erase Eol;
-    );;
+    )
 
-let sleepf (sec: float) =
-    ignore (Unix.select [] [] [] sec)
-
-let render (progress : int) (goal: int) =
-    let max_width = get_terminal_columns () in
-    let percentage = (float_of_int progress) /. (float_of_int goal) in
-    let width = int_of_float (percentage *. (float_of_int max_width)) in
-    (*print_string "rendered"; print_int width;*)
+let render line =
     clear_line ();
-    ANSITerminal.(print_string [magenta; on_white] ((str_n_times "⑄" (max (width - 8) 0)) ^ "🐫 "));
+    ANSITerminal.(print_string [] line);
     flush stdout;;
 
-let rec main progress goal =
-    render progress goal;
-    print_int progress;
-    sleepf 0.1;
-    if progress < 100 then
-    main (progress + 1) goal;;
 
-let () = main 0 100;
+(* make the progress bar *)
+
+let rec str_n_times str n =
+    if n < 1 then "" else str ^ (str_n_times str (n - 1));;
+
+(* progress expected to be between 0 and 1 *)
+let string_of_progress progress =
+    let total_width = get_terminal_columns () in
+    let progress_width = max 0 (int_of_float (progress *. (float_of_int total_width))) in
+    let goal_width = max 0 (total_width - progress_width) in
+    (* use same characters as https://github.com/yarnpkg/yarn *)
+    let progress_bar = str_n_times "█" progress_width in
+    let remaining_bar = str_n_times "░" goal_width in
+    progress_bar ^ remaining_bar
+
+
+(* demo *)
+
+let sleepf (sec: float) = ignore (Unix.select [] [] [] sec)
+
+let rec main (progress: float) =
+    render (string_of_progress progress);
+    sleepf 0.05;
+    if progress <= 1.0 then main (progress +. 0.05)
+
+let () = main 0.;
